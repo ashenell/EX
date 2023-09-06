05c7381e534a753a4048061f3b3e0efc97f328ab
Ashenell <rauno.vaher123@gmail.com>
2023-09-06 19:42:08 +0300

    removed spaces

--- ex01_hello/hello.py
+++ ex01_hello/hello.py
@@ -1,15 +1,9 @@
-
-
 def helloWorld(word: str):
     """Hello World!
-
     Args:
         word (str): Give a string argument for a function
-
     Returns:
         string: example "Hello world!
     """
     return print(word)
-
-
 helloWorld("Hello world!")

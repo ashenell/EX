0e0aa17df6fd2da1c70453486d077d9ec1c009b0
Ashenell <rauno.vaher123@gmail.com>
2023-09-06 19:40:30 +0300

    Hello world first function

---
+++ ex01_hello/hello.py
@@ -0,0 +1,15 @@
+
+
+def helloWorld(word: str):
+    """Hello World!
+
+    Args:
+        word (str): Give a string argument for a function
+
+    Returns:
+        string: example "Hello world!
+    """
+    return print(word)
+
+
+helloWorld("Hello world!")
